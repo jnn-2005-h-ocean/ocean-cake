09f4faad0f980e378ad8b8efdf11a16dfdafbd52
takuma39 <takuma.sato.job@gmail.com>
2020-06-17 10:55:23 +0000

    deviceのcontrollerとview作成 / deviceのrouting作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+}
+
+devise_scope :admins do
+		get 'admins/sign_in' => 'admins/sessions#create'
+		get 'admins/sign_out' => 'admins/sessions#destroy'
+end
+
+devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+}
+
+devise_scope :customers do
+		get 'customers/sign_in' => 'customers/sessions#create'
+		get 'customers/sign_out' => 'customers/sessions#destroy'
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
