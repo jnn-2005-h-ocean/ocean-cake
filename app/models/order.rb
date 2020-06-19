16c172c99a1b245ee14e167a5d7d78188147329f
渡辺有智 <aritomo@aritomo.local>
2020-06-19 14:10:59 +0900

    Add association And enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,19 @@
 class Order < ApplicationRecord
+
+	has_many :order_items
+	belongs_to :customer
+
+	enum order_status:{
+    wait_deposit: 0, #入金待ち
+    payment_confirmation: 1, #入金確認
+    inproduction: 2, #制作中
+    preparing_for_shipment: 3, #発送準備中
+    sent: 4, #発送済み
+  }
+
+  enum payment_method:{
+    bank_transfer: 0, #銀行振込
+    credit: 1, #クレジット払い
+  }
+
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,8 @@
 class OrderItem < ApplicationRecord
+
+	belongs_to :product
+	belongs_to :order
+
+	enum making_status:{cannot: 0, waiting: 1, doing: 2, done: 3}
+
 end
