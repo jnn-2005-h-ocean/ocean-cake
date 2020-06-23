ff30967672054e9d7245e454ce09d7947b476be6
Your Name <ishiguro-tomoya@MacBook-Pro.lan>
2020-06-23 10:36:14 +0900

    [Add]admin_customer_view

--- app/controllers/customers/order_items_controller.rb
+++ app/controllers/customers/order_items_controller.rb
@@ -2,8 +2,12 @@ class Customers::OrderItemsController < ApplicationController
 	layout 'customers'
 
 	def index
+		@orders = Order.where(customer_id: current_customer.id).order(created_at: "DESC")
+
 	end
 
 	def show
+		@order = Order.find(params[:id])
+		@order_details = OrderDetail.where(order_id: params[:id])
 	end
 end
