db5f3e2332d30754f3cf4f09e76ef47343f22ed0
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-06-25 12:04:44 +0900

    [Add admins/homes/top]

--- app/controllers/admins/homes_controller.rb
+++ app/controllers/admins/homes_controller.rb
@@ -1,8 +1,10 @@
 class Admins::HomesController < ApplicationController
+	before_action :authenticate_admin!
 	layout 'admins'
+
 	def top
-	# 	range = Date.today.beginning_of_day..Date.today.end_of_day
-	# 　　 @range = OrderItem.where(created_at: range)
+		range = Date.today.beginning_of_day..Date.today.end_of_day
+		@range = OrderItem.where(created_at: range)
 	end
 
 end
