ca2f1bcf83ccbb12345c2dcf7d93ad4c3c13456f
渡辺有智 <aritomo@aritomo.local>
2020-06-25 18:46:03 +0900

    Add order history & made admins order contorller

--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -1,13 +1,21 @@
 class Admins::OrderItemsController < ApplicationController
 	layout 'admins'
 
-	def index
-	end
-
-	def show
-	end
-
 	def update
+		order_item = OrderItem.find(params[:id])
+		if order_item.update(order_item_params)
+			flash[:update] = "制作ステータスを更新しました"
+			redirect_to admins_order_path(order_item.order)
+		else
+			render "show"
+		end
 	end
+
+
+	private
+
+	def order_item_params
+  		params.require(:order_item).permit(:making_status)
+  	end
 
 end
