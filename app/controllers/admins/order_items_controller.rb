3b501606e200bf96d5807339575f99102e79b8ad
渡辺有智 <aritomo@aritomo.local>
2020-06-27 16:29:22 +0900

    fix order status and admins product routes

--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -4,14 +4,21 @@ class Admins::OrderItemsController < ApplicationController
 
 
 	def update
-		order_item = OrderItem.find(params[:id])
-		if order_item.update(order_item_params)
-			flash[:update] = "制作ステータスを更新しました"
-			redirect_to admins_order_path(order_item.order)
-		else
-			render "show"
+		@order_item = OrderItem.find(params[:id])
+		@order_item.update(order_item_params)
+		@order = @order_item.order
+
+		if @order_item.making_status == "制作中"
+			@order.update(order_status: 2)
 		end
+
+		if @order_item.making_status == "制作完了"
+			@order.update(order_status: 3)
+		end
+		redirect_to admins_order_path(@order.id)
+
 	end
+
 
 
 	private
@@ -19,5 +26,9 @@ class Admins::OrderItemsController < ApplicationController
 	def order_item_params
   		params.require(:order_item).permit(:making_status)
   	end
+
+  	def order_params
+		params.require(:order).permit(:order_status)
+	end
 
 end
